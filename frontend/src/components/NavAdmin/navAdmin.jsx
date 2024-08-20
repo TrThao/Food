f84aa3be785d3db822b03ca2bc629ac0e9fa0a54
TrThao <thaotranit.hubt@gmail.com>
2024-08-20 11:18:49 +0700

    abc

---
+++ frontend/src/components/NavAdmin/navAdmin.jsx
@@ -0,0 +1,11 @@
+import React from "React"
+import './NavAdmin.css'
+const NavAdmin = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default NavAdmin
---
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+imoport './Sidebar.css'
+const Sidebar = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Sidebar
